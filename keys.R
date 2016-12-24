b28fa343d2fc654075c40093c270bf4318b6ffde
hestiri <estiri.hossein@gmail.com>
2016-12-23 21:38:29 -0500

    set up keys for ODBC/JDBC connection

---
+++ keys.R
@@ -0,0 +1,20 @@
+### This script is supposed to be confidential and only used by the person it is generated for.
+
+## what is your username for JDBC/ODBC connection?
+usrnm <- "SET YOUR USERNAME"
+
+## what is your password for JDBC/ODBC connection?
+pss <- "SET YOUR PASSWORD"
+
+# who is running the test?
+tester <- "SET THE TESTER NAME" 
+
+## reading organization name
+org <- org
+
+###identifying data model PCORnet V3, OMOP V4-V5
+CDM <- CDM
+
+###identifying SQL connection PostgreSQL or SQL Server
+SQL <- SQL
+
