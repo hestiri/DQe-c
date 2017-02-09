be55a64a1dc5e4aa96b90e746e61ba6b579310a0
hestiri <estiri.hossein@gmail.com>
2017-02-08 23:55:07 -0500

    data model execution script

--- dmrun.R
+++ dmrun.R
@@ -16,20 +16,12 @@ if (CDM == "PCORNET3") {
   DQTBL_KEYS$Index <- "Count_In"
   dmtest <- parse(file = "dmtest_pcornet3.R")
   
-} else
-  if (CDM == "OMOPV5") {
-    #this line need update for omopv5 ---> DQTBL_KEYS <- select(subset(DQTBL, ColNam %in% c("person_id","care_site_id","visit_occurrence_id","location_id","organization_id")),TabNam, ColNam, UNIQFRQ)
-    ## creating an index for plotting: Count In means number rof unique frequencies that exist in the reference table
-    DQTBL_KEYS$Index <- "Count_In"
-    dmtest <- parse(file = "dmtest_omop5.R")
-  }
-
-
+} 
 
 
 for (i in seq_along(dmtest)) {
   tryCatch(eval(dmtest[[i]]), 
-           error = function(e) message("No Worries!! ;) Hossein said it is fine if there is an ", as.character(e)))
+           error = function(e) message("No Worries!! HE thinks it is fine if there is an ", as.character(e)))
 }
 
 
