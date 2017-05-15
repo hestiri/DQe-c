7be5ca49ee5f65083d485edba1f2606396c9b907
hestiri <estiri.hossein@gmail.com>
2017-05-15 13:16:05 -0400

    made keys.R safer!

--- keys.R
+++ keys.R
@@ -1,13 +1,48 @@
 ### This script is supposed to be confidential and only used by the person it is generated for.
 
-## what is your username for JDBC/ODBC connection?
-usrnm <- "SET YOUR USERNAME"
+if (!require("tcltk")) install.packages('tcltk')
 
-## what is your password for JDBC/ODBC connection?
-pss <- "SET YOUR PASSWORD"
+
+getLoginDetails <- function(){
+  ## Based on code by Barry Rowlingson
+  ## http://r.789695.n4.nabble.com/tkentry-that-exits-after-RETURN-tt854721.html#none
+  require(tcltk)
+  tt <- tktoplevel()
+  tkwm.title(tt, "Get login details")
+  Name <- tclVar("Login ID")
+  Password <- tclVar("Password")
+  entry.Name <- tkentry(tt,width="20", textvariable=Name)
+  entry.Password <- tkentry(tt, width="20", show="*", 
+                            textvariable=Password)
+  tkgrid(tklabel(tt, text="Please enter your login details."))
+  tkgrid(entry.Name)
+  tkgrid(entry.Password)
+  
+  OnOK <- function()
+  { 
+    tkdestroy(tt) 
+  }
+  OK.but <-tkbutton(tt,text=" Login ", command=OnOK)
+  tkbind(entry.Password, "<Return>", OnOK)
+  tkgrid(OK.but)
+  tkfocus(tt)
+  tkwait.window(tt)
+  
+  invisible(c(loginID=tclvalue(Name), password=tclvalue(Password)))
+}
+credentials <- getLoginDetails()
+## Do what needs to be done
+
+
+
+## username for JDBC/ODBC connection?
+usrnm = credentials[[1]]
+
+## password for JDBC/ODBC connection
+pss = credentials[[2]]
 
 # who is running the test?
-tester <- "SET THE TESTER NAME" 
+tester <- "TESTER NAME" 
 
 ## reading organization name
 org <- org
